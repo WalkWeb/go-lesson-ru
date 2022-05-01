d187d88615d404a472ebd96b41d275a77d035829
WalkWeb <av18775@list.ru>
2022-05-01 16:28:50 +0300

    Added Lesson #3: Variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,28 @@
+
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "string"
+    fmt.Println(a)
+
+    var b string = "string again"
+    fmt.Println(b)
+
+    var c string
+    fmt.Println(c)
+
+    var d, e int = 1, 2
+    fmt.Println(d, e)
+
+    var f int
+    fmt.Println(f)
+
+    var x bool
+    fmt.Println(x)
+
+    s := "apple"
+    fmt.Println(s)
+}
